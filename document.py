65acb4ea9d182003f0eea037cb0de0d89d5e873a
Yaroslavl Hnatiuk <jarukgnatiuk@gmail.com>
2025-09-26 13:14:00 +0300

    Edit protected fields into private

--- document.py
+++ document.py
@@ -1,17 +1,17 @@
 from abc import ABC, abstractmethod
 
 class Document(ABC):
-    _count = 0
+    __count = 0  # приватне статичне поле
 
     def __init__(self, doc_type, doc_name, doc_date):
-        self._doc_type = doc_type
-        self._doc_name = doc_name
-        self._doc_date = doc_date
-        Document._count += 1
+        self.__doc_type = doc_type
+        self.__doc_name = doc_name
+        self.__doc_date = doc_date
+        Document.__count += 1
 
-    def getDocType(self): return self._doc_type
-    def getDocName(self): return self._doc_name
-    def getDocDate(self): return self._doc_date
+    def getDocType(self): return self.__doc_type
+    def getDocName(self): return self.__doc_name
+    def getDocDate(self): return self.__doc_date
 
     @abstractmethod
     def showDocInfo(self):
@@ -19,7 +19,8 @@ class Document(ABC):
 
     @staticmethod
     def getDocumentCount():
-        return Document._count
+        return Document.__count
+
 
 class Printable:
     def print_to_console(self, text):
@@ -31,17 +32,18 @@ class Printable:
 class Report(Document, Printable):
     def __init__(self, doc_type, doc_name, doc_date, report_author, report_viewer):
         super().__init__(doc_type, doc_name, doc_date)
-        self._report_author = report_author
-        self._report_viewer = report_viewer
+        self.__report_author = report_author
+        self.__report_viewer = report_viewer
 
-    def changeViewer(self, new_viewer): self._report_viewer = new_viewer
+    def changeViewer(self, new_viewer):
+        self.__report_viewer = new_viewer
 
     def showDocInfo(self):
-        info = (f"Тип: {self._doc_type}\n"
-                f"Назва: {self._doc_name}\n\n"
-                f"Автор: {self._report_author}\n"
-                f"Адресат: {self._report_viewer}\n\n"
-                f"Дата створення: {self._doc_date}")
+        info = (f"Тип: {self.getDocType()}\n"
+                f"Назва: {self.getDocName()}\n\n"
+                f"Автор: {self.__report_author}\n"
+                f"Адресат: {self.__report_viewer}\n\n"
+                f"Дата створення: {self.getDocDate()}")
 
         self.print_to_console(info)
 
@@ -49,20 +51,19 @@ class Report(Document, Printable):
 class Contract(Document, Printable):
     def __init__(self, doc_type, doc_name, doc_date, first_party, second_party, expiration_date):
         super().__init__(doc_type, doc_name, doc_date)
-        self._contract_first_party = first_party
-        self._contract_second_party = second_party
-        self._contract_expiration_date = expiration_date
+        self.__contract_first_party = first_party
+        self.__contract_second_party = second_party
+        self.__contract_expiration_date = expiration_date
 
     def extendContractExpirationDate(self, new_expiration_date):
-        self._contract_expiration_date = new_expiration_date
+        self.__contract_expiration_date = new_expiration_date
 
     def showDocInfo(self):
-        info = (f"Тип: {self._doc_type}\n"
-                f"Назва: {self._doc_name}\n\n"
-                f"Перша сторона: {self._contract_first_party}\n"
-                f"Друга сторона: {self._contract_second_party}\n\n"
-                f"Дата укладення контракту: {self._doc_date}\n"
-                f"Дата закінчення контракту: {self._contract_expiration_date}")
+        info = (f"Тип: {self.getDocType()}\n"
+                f"Назва: {self.getDocName()}\n\n"
+                f"Перша сторона: {self.__contract_first_party}\n"
+                f"Друга сторона: {self.__contract_second_party}\n\n"
+                f"Дата укладення контракту: {self.getDocDate()}\n"
+                f"Дата закінчення контракту: {self.__contract_expiration_date}")
 
         self.print_to_console(info)
-
